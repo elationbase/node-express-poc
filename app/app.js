10d51aee08af4d0d91a0549445fc4c3e1a04de40
Alvaro Montero <elationbase@gmail.com>
2016-09-19 10:54:34 -0400

    load JSON data

--- app/app.js
+++ app/app.js
@@ -1,9 +1,38 @@
-const http = require('http');
-const server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type" : "text/html"});
-    response.write('<h1>elationbase Meetups</h1>');
-    response.end();
+/* jshint esversion: 6 */
+var express = require('express');
+
+var app = express();
+var data = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+    var info = '';
+    data.speakers.forEach(function(item) {
+        info += `
+            <li>
+                <h2>${item.name}<h2>
+                <p>${item.summary}</p>
+            </li>
+        `;
+    });
+    res.send(`
+        <h1>elationbase Meetups</h1>
+        ${info}
+    `);
 });
 
-server.listen(3000);
-console.log('listening in http://localhost:3000');
+var server = app.listen(app.get('port'), function() {
+    console.log('listening in http://localhost:' + app.get('port'));
+});
+
+
+// const http = require('http');
+// const server = http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type" : "text/html"});
+//     response.write('<h1>elationbase Meetups</h1>');
+//     response.end();
+// });
+//
+// server.listen(3000);
+// console.log('listening in http://localhost:3000');
